9f5d847d9d7b6ff5fe20b15c24701176610b2c1a
Gabriel Ron <gaboarp2020@gmail.com>
2019-02-15 13:32:40 -0400

    resolving conglict

--- public/js/app.js
+++ public/js/app.js
@@ -20670,8 +20670,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/juancpaiva/laravel-projects/tecno-majestic-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/juancpaiva/laravel-projects/tecno-majestic-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\gaboa\code\tecno-majestic-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\gaboa\code\tecno-majestic-app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
